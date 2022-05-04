a42cb8bf95acc67eeae2e330dce1fe366672e6e6
Ali Mohammadi <ali.mohammadi@vbt.com.tr>
2022-05-04 23:08:00 +0300

    components

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Message = ()=> {
+  return (
+  <Text style={{fontSize:30, padding: 50}}>
+    Hello React Native
+  </Text>
+  )
+}
+
+export default Message;
