e23fff01183aa511f2a5c479b779f92a0f0271ca
Ali Mohammadi <ali.mohammadi@vbt.com.tr>
2022-05-06 11:12:04 +0300

    props

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
-const Message = ()=> {
+const Message = (props)=> {
   return (
   <Text style={{fontSize:30, padding: 50}}>
-    Hello React Native
+    {props.name}
   </Text>
   )
 }
 
-export default Message;
+
+const AllMessages = ()=> {
+  return(
+  <View>
+    <Message name="Hello Ract Native"/>
+    <Message name="Hello Java"/>
+    <Message name="Hello Python"/>
+  </View>
+  )
+ 
+}
+
+export default AllMessages;
